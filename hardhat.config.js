b635ec86aa9d7ed595fd2e8f60a9db0cf1988eb2
eggy2264 <127124583+eggy2264@users.noreply.github.com>
2024-03-26 23:51:16 +0800

    Add files via upload

---
+++ hardhat.config.js
@@ -0,0 +1,18 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan"); // Add this line
+require("@nomiclabs/hardhat-ethers");
+require('dotenv').config();
+
+module.exports = {
+  solidity: "0.8.24",
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [`${process.env.PRIVATE_KEY}`]
+    },
+    // ... other network configs
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY // Your Etherscan API key
+  },
+};
