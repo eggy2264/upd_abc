b5046c2a7bcf67e3dd6e74a4027dd4d475b88b44
eggy2264 <juddberin@gmail.com>
2024-03-27 23:07:33 +0800

    updated url

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,7 +7,7 @@ module.exports = {
   solidity: "0.8.24",
   networks: {
     sepolia: {
-      url: `https://eth-sepolia.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+      url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
       accounts: [`${process.env.PRIVATE_KEY}`]
     },
     // ... other network configs
